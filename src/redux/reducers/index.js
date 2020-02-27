85c03c1a408df26e74f574c4d97fb73184288f65
Meghan Gunderson <megundersonmn@gmail.com>
2020-02-27 10:36:21 -0600

    created getOwnersReducer and updated other reducers

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,15 @@
+import { combineReducers } from 'redux';
+import getOwnersReducer from './getOwnersReducer';
+import getPetsReducer from './getPetsReducer';
+
+// rootReducer is the primary reducer for our entire project
+// It bundles up all the other reducers so our project can use them.
+// This is imported in index.js as rootSaga
+
+const rootReducer = combineReducers({
+    getOwnersReducer,
+    getPetsReducer,
+})
+    
+
+
